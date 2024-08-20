6013edf99929973299faccdc4b7ed16fbc53cbab
Adrian <akoziskie@gmail.com>
2024-08-20 11:51:22 -0400

    html-webpack-plugin for multiple js entry points

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,38 @@
 const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	entry: "./src/main.js",
-	output: {
-		filename: "main.js",
-		path: path.resolve(__dirname, "public"),
+	entry: {
+		index: "./src/index.js",
+		projects: "./src/projects.js",
+		triangle: "./src/triangle.js"
 	},
-	mode: "development"
+	output: {
+		filename: "[name].bundle.js",
+		path: path.resolve(__dirname, "public"),
+		//clean: true
+	},
+	plugins: [
+		new htmlWebpackPlugin({
+			template: "./src/index.html",
+			filename: "index.html",
+			chunks: ["index"]
+		}),
+		new htmlWebpackPlugin({
+			template: "./src/projects.html",
+			filename: "projects.html",
+			chunks: ["projects"]
+		}),
+		new htmlWebpackPlugin({
+			template: "./src/info.html",
+			filename: "info.html",
+			chunks: []
+		}),
+		new htmlWebpackPlugin({
+			template: "./src/triangle.html",
+			filename: "triangle.html",
+			chunks: ["triangle"]
+		})
+	],
+	mode: "development"  // or production
 };
