8cb3041fd934008fb45310b2c93c8659d9bc7245
Adrian <akoziskie@gmail.com>
2024-09-07 02:03:04 -0400

    reworked ASCII ply loader

--- src/js/plyReader.js
+++ src/js/plyReader.js
@@ -1,6 +1,76 @@
-// NOTE: this will not read ALL .ply files (but it works for my purposes)
+import { AssetLoadError } from "./errors";
 
-async function readPly(url, topology) {
+function readPlyHeader(lines) {
+    if (lines[0] != "ply") {
+        throw new AssetLoadError("Invalid ply file");
+    }
+
+    const metadata = {};
+    metadata.elements = [];
+
+    let dataStart = 0;
+    let element;
+    for (const line of lines) {
+        dataStart++;
+        if (line === "end_header") { break; }
+        const parts = line.split(" ");
+        if (parts[0] === "format") {
+            metadata.format = parts[1];  // ascii, binary_little_endian, binary_big_endian
+            metadata.formatVersion = parts[2];
+        }
+        else if (parts[0] === "element") {
+            metadata.elements[parts[1]] = {
+                count: Number(parts[2]),
+                properties: [],
+            };
+            element = parts[1];
+        }
+        else if (parts[0] === "property") {
+            if (parts[1] === "list") {
+                // TODO handle list
+                metadata.elements[element].properties.push({
+                    type: parts[1],
+                    countType: parts[2],
+                    listType: parts[3],
+                    name: parts[4],
+                });
+            }
+            else {
+                metadata.elements[element].properties.push({
+                    name: parts[2],
+                    type: parts[1],
+                });
+            }
+        }
+    }
+    metadata.dataStart = dataStart;
+    return metadata;
+}
+
+function readASCII(lines, metadata) {
+    const data = {};
+
+    let i = metadata.dataStart;
+    for (const element in metadata.elements) {
+        data[element] = [];
+        for (let j = 0; j < metadata.elements[element].count; j++) {
+            if (metadata.elements[element].properties[0].type === "list") {
+                const parts = lines[i+j].split(" ");
+                data[element].push(parts.slice(1, parts[0] + 1));
+            }
+            else {
+                data[element].push(lines[i+j].split(" "));
+            }
+        }
+        i += metadata.elements[element].count;
+    }
+
+    return data;
+}
+
+function readBinary(lines, metadata) {}  // TODO
+
+async function readPly(url) {
     // fetch ply file from server;
     const response = await fetch(url);
     const text = await response.text();
@@ -8,95 +78,45 @@ async function readPly(url, topology) {
     // separate into lines
     const lines = text.split("\n");
 
-    const data = {};
-    data.properties = [];
-    data.faces = [];
-    data.topologyVerts = 0;  // number of vertices for rendering topology
-    data.source = url;
+    // read header
+    const metadata = readPlyHeader(lines);
 
-    let readingVertices = false;
-    let readingFaces = false;
-    let vertsCount = 0;
-    let faceCount = 0;
-
-    for (const line of lines) {
-        const parts = line.split(" ");
-        if (readingFaces) {
-            // read faces
-            const sides = Number(parts[0]);
-            data.faces.push(parts.slice(1, sides + 1).map(Number));
-            // add vertices to count
-            if (topology === "triangle-list") { data.topologyVerts += (sides - 2) * 3; }
-            else if (topology === "line-list") { data.topologyVerts += sides * 2; }
-            // count faces
-            if (--faceCount === 0) {
-                readingFaces = false;
-            }
-        }
-        else if (readingVertices) {
-            // read vertices
-            for (const i in parts) {
-                data[data.properties[i]].push(Number(parts[i]));
-            }
-            // count vertices
-            if (--vertsCount === 0) {
-                readingVertices = false;
-                readingFaces = true;
-            }
-        }
-        else {
-            // read header
-            // data format
-            if (parts[0] == "format" && parts[1] === "binary") {
-                throw new Error("Binary .ply files not supported");
-            }
-            // element count
-            else if (parts[0] == "element") {
-                if (parts[1] == "vertex") { vertsCount = Number(parts[2]); }
-                if (parts[1] === "face") { faceCount = Number(parts[2]); }
-            }
-            // properties
-            else if (parts[0] == "property") {
-                if (parts[1] == "list") {}  // TODO for evil files
-                else {
-                    // parts[1] is type
-                    data.properties.push(parts[2]);
-                }
-            }
-            // end_header
-            else if (parts[0] == "end_header") {
-                readingVertices = true;
-                // create fields in data Object
-                for (const property of data.properties) { data[property] = []; }
-            }
-        }
+    // read data
+    let data;
+    if (metadata.format === "ascii") {
+        data = readASCII(lines, metadata);
     }
+    else if (metadata.format === "binary_little_endian") {}
+    else if (metadata.format === "binary_big_endian") {}
+    else { throw new AssetLoadError("Invalid ply format"); }
 
-    return data;
+    return { data, metadata };
 }
 
 export async function plyToTriangleList(url) {
-    const data = await readPly(url, "triangle-list");
+    const { data, metadata } = await readPly(url);
+
     // generate Float32Array
-    const vertices = {};
-    vertices.properties = data.properties;
-    vertices.floats = new Float32Array(data.topologyVerts * data.properties.length);
+    const vertices = {};  // TODO include other data (e.g. material)
+    vertices.properties = metadata.elements.vertex.properties.map(p => p.name);
+    const topologyVerts = data.face.reduce((sum, a) => sum + (a.length - 2) * 3, 0);
+    vertices.floats = new Float32Array(topologyVerts * vertices.properties.length);
     let vIndex = 0;
-    for (const face of data.faces) {
+    for (const face of data.face) {
         // TODO selectable triangulation behaviour
         // handle polygons with fan of triangles
         for (let i = 1; i < face.length - 1; i++) {
             // v0
-            for (const property of vertices.properties) {
-                vertices.floats[vIndex++] = data[property][face[0]];
+            for (const property in vertices.properties) {
+                vertices.floats[vIndex++] = data.vertex[face[0]][property];
             }
             // vi
-            for (const property of vertices.properties) {
-                vertices.floats[vIndex++] = data[property][face[i]];
+            for (const property in vertices.properties) {
+                vertices.floats[vIndex++] = data.vertex[face[i]][property];
             }
             // v(i+1)
-            for (const property of vertices.properties) {
-                vertices.floats[vIndex++] = data[property][face[i+1]];
+            for (const property in vertices.properties) {
+                vertices.floats[vIndex++] = data.vertex[face[i+1]][property];
             }
         }
     }
